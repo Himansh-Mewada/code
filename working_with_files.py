9476920c6b3e0d2c40916ed957682c78ff53fe45
Himansh Mewada <himanshcode@gmail.com>
2025-06-04 21:30:23 +0530

    Update working_with_files.py

--- working_with_files.py
+++ working_with_files.py
@@ -1,15 +1,19 @@
+# write
 with open("my_notes.txt", "w") as file:
     file.write("My first note on python.\n")
     file.write("File handling is interesting.\n")
     file.write("Always remember to close files.")
 
+# append
 with open("my_notes.txt", "a") as file:
     file.write("\nand the with statement is best practice.")
 
+# read the whole content
 with open("my_notes.txt", "r") as file:
     return_string = file.read()
     print(return_string)
-
+    
+# read line by line
 with open("my_notes.txt", "r") as file:
     for line in file:
         print(line, end='')
