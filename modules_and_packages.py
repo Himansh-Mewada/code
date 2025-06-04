d95aeae38b14f860fd0ab7ea89d5a09a3fa2f0b3
Himansh Mewada <himanshcode@gmail.com>
2025-06-04 22:26:22 +0530

    Create modules_and_packages.py

---
+++ modules_and_packages.py
@@ -0,0 +1,9 @@
+import greetings_module
+greetings_module.say_hi("Himansh")
+print(greetings_module.DEFAULT_LANGUAGE)
+
+from greetings_module import say_hi
+say_hi("Friend")
+
+import datetime
+print(datetime.datetime.now())
